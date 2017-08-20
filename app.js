bedfb1727c22bdba3cfd8c5cf351db318fd60704
a-bort <bortron5000@gmail.com>
2017-08-20 16:23:08 -0400

    MEAN stack

---
+++ app.js
@@ -0,0 +1,37 @@
+
+/**
+ * Module dependencies.
+ */
+
+var config = require('./config/config')
+
+  var express = require('express')
+  , app = express()
+  , port = process.env.PORT || config.port
+  , path = require('path')
+  , mongoose = require('mongoose')
+
+  , bodyParser = require('body-parser')
+  , favicon = require('serve-favicon');
+
+//public path
+app.use(express.static(path.join(__dirname, 'public')));
+
+ //view stuff
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
+var uri = config.dbUri;
+
+var db = mongoose.connect(uri, {useMongoClient: true });
+
+// all environments
+app.use(bodyParser());
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
+//routes
+require('./routes.js')(app);
+
+app.listen(port);
+console.log("Listening on " + port);
---
+++ config/config.js
@@ -0,0 +1,6 @@
+exports.dbUri = process.env.MONGOLAB_URI;
+exports.port = process.env.PORT;
+
+exports.dev = process.env.DEV;
+
+exports.baseUrl = process.env.BASE_URL;
